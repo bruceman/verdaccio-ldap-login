31b6f522a7aa31e756f8c01532d2da38f58f678c
bruce.li <bruce.li@coupang.com>
2020-05-16 13:32:13 +0800

    improve ldap auth

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+# Change Log
+
+## 0.1.0
+- don't allow adduser and changepassword action
+- optimize the authiticate performance
+
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const ldap = require('ldapjs');
  */
 class AuthLdapPlugin {
     constructor(config, options) {
+      this.users = {};
+      this.url = config.url || 'ldap://127.0.0.1:1389';
       this.dn = config.dn || 'uid={},dc=com';
-      this.client = ldap.createClient({url: config.url});
     }
   
     /**
@@ -23,17 +24,40 @@ class AuthLdapPlugin {
         // jsut fail directly
         cb(null, false);
       }
+
+      // already login
+      if (this.users[user] === password){
+        cb(null, [user]);
+        return;
+      }
   
-      this.client.bind(this.dn.replace('{}', user), password, function (err) {
+      const client = ldap.createClient({url: this.url});
+      // try login from ldap
+      client.bind(this.dn.replace('{}', user), password, (err) => {
         if (err) {
-          console.log('login failed: ' + err);
+          console.log(`[${user}] login failed: ${err}`);
           // fail
           cb(null, false);
         } else {
           // success
+          this.users[user] = password;
           cb(null, [user]);
         }
       });
+    }
+
+    /**
+     * don't allow add user directly
+     */
+    adduser(user, password, cb) {
+      cb(null, false);
+    }
+
+    /**
+     * don't allow change password
+     */
+    changePassword(user, password, newPassword, cb) {
+      cb(null, false);
     }
   }
   
